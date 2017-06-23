be45c45cdcf198bdba6caae063439e8c59f5ecd6
neskov7 <giuseppenesca.g@gmail.com>
2017-06-23 16:56:30 +0000

    R1 finished

---
+++ elezioni/Elettore.java
@@ -0,0 +1,45 @@
+package elezioni;
+
+public class Elettore implements Cittadino {
+	
+	private String nome, cognome;
+	boolean haVotato = false;
+	boolean isCapolista = false;
+	boolean isCandidato = false;
+	
+	public Elettore(String nome, String cognome){
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+
+	@Override
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public String getCognome() {
+		return cognome;
+	}
+
+	@Override
+	public boolean haVotato() {
+		return haVotato;
+	}
+
+	@Override
+	public boolean isCapolista() {
+		return isCapolista;
+	}
+
+	@Override
+	public boolean isCandidato() {
+		return isCandidato;
+	}
+
+	@Override
+	public long getNumeroVoti() {
+		return 0;
+	}
+
+}
